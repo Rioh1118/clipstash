b4cab726b8f56f20c43d6d75660e7190b9c3c248
Rioh1118 <rio2515t@gmail.com>
2024-01-16 23:21:14 +0900

    add files of clip module

--- src/lib/domain/clip/mod.rs
+++ src/lib/domain/clip/mod.rs
@@ -1,1 +1,15 @@
+use serde::{Deserialize, Serialize};
+
 pub mod field;
+
+#[derive(Debug, Clone,Deserialize,Serialize)]
+pub struct Clip{
+    pub clip_id: crate::domain::clip::field::ClipId,
+    pub shortcode: crate::domain::clip::field::Shortcode,
+    pub content: crate::domain::clip::field::Content,
+    pub title: crate::domain::clip::field::Title,
+    pub posted: crate::domain::clip::field::Posted,
+    pub expires: crate::domain::clip::field::Expires,
+    pub password: crate::domain::clip::field::Password,
+    pub hits: crate::domain::clip::field::Hits,
+}
