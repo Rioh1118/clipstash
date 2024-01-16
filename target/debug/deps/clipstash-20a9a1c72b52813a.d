76e8a38bb04f86acf8016144183a8ffac232d579
Rioh1118 <rio2515t@gmail.com>
2024-01-16 19:22:08 +0900

    first commit

---
+++ target/debug/deps/clipstash-20a9a1c72b52813a.d
@@ -0,0 +1,11 @@
+/Users/riohatta/Desktop/ztm-rust/clipstash/target/debug/deps/libclipstash-20a9a1c72b52813a.rmeta: src/lib/mod.rs src/lib/data/mod.rs src/lib/domain/mod.rs src/lib/domain/clip/mod.rs src/lib/domain/clip/field/mod.rs src/lib/service/mod.rs src/lib/web/mod.rs
+
+/Users/riohatta/Desktop/ztm-rust/clipstash/target/debug/deps/clipstash-20a9a1c72b52813a.d: src/lib/mod.rs src/lib/data/mod.rs src/lib/domain/mod.rs src/lib/domain/clip/mod.rs src/lib/domain/clip/field/mod.rs src/lib/service/mod.rs src/lib/web/mod.rs
+
+src/lib/mod.rs:
+src/lib/data/mod.rs:
+src/lib/domain/mod.rs:
+src/lib/domain/clip/mod.rs:
+src/lib/domain/clip/field/mod.rs:
+src/lib/service/mod.rs:
+src/lib/web/mod.rs:
